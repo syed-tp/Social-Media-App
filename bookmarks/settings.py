37b65e8a906f8f64bd0c83a32fb5be5a0783558d
syed <syed@testpress.in>
2024-08-28 12:38:37 +0530

    Add: model and view for images

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'django_extensions',    
+    'django_extensions',
+    'images.apps.ImagesConfig',
     
 ]
 
